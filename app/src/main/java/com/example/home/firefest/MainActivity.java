307b719d0aa1e5e888db8e9bd508980110d836a1
storyteller <shashanksharma580S@gmail.com>
2017-06-21 13:36:12 +0530

    shashank

--- app/src/main/java/com/example/home/firefest/MainActivity.java
+++ app/src/main/java/com/example/home/firefest/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("shasahnk");
+        System.out.println("shasahnk");
+
     }
 }
